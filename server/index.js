7aa0a59b25fbc629b7158732f3934ef926b89747
catdogmat <45089908+catdogmat@users.noreply.github.com>
2021-09-07 14:26:48 +1200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -66,6 +66,6 @@ app.post('/', upload.single('myfile'), async(req, res) => {
 app.use('/api', router);
 
 
-app.listen(5000,()=>{
-    console.log("\u{1F525}\u{1F680} app listen on port",5000,"\u{1F525}\u{1F680}")
+app.listen(3001,()=>{
+    console.log("\u{1F525}\u{1F680} app listen on port",3001,"\u{1F525}\u{1F680}")
  });
